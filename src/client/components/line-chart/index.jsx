fd99c446465534c35bb1385da08e4e0af977b091
Ferenc Nagy <nagy.ferenc@kodinformatika.hu>
2017-02-24 16:22:35 +0100

    #2 Linechart improvements with grids

--- src/client/components/line-chart/index.jsx
+++ src/client/components/line-chart/index.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import * as d3 from 'd3';
 import './line-chart.less';
 import * as _ from 'lodash';
@@ -7,6 +7,9 @@ class LineChart extends React.Component {
     componentDidMount() {
         this.svg = d3.select(this.root).append('svg').attr('width', '100%').attr('height', '100%');
 
+        this.gridX = this.svg.append('g').attr('class', 'grid grid-x');
+        this.gridY = this.svg.append('g').attr('class', 'grid grid-y');
+
         this.line = d3.line()
             .x((d, i) => {
                 return this.scaleX(i)
@@ -14,15 +17,6 @@ class LineChart extends React.Component {
             .y((d) => {
                 return this.scaleY(d.value);
             });
-
-        this.svg.append('path')
-            .datum([])
-            .attr('fill', 'none')
-            .attr('stroke', 'steelblue')
-            .attr('stroke-linejoin', 'round')
-            .attr('stroke-linecap', 'round')
-            .attr('stroke-width', 1.5)
-            .attr('d', this.line);
 
         this.bound_renderChart = this.renderChart.bind(this);
         window.addEventListener('resize', this.bound_renderChart);
@@ -36,18 +30,46 @@ class LineChart extends React.Component {
         this.renderChart();
     }
 
+    getBounds() {
+        return this.root.getBoundingClientRect();
+    }
+
     renderChart() {
         if (this.props.series && this.props.series.length > 0) {
             this.scaleX = d3.scaleLinear()
-                .range([0, this.root.getBoundingClientRect().width])
+                .range([0, this.getBounds().width])
                 .domain([0, this.props.series.length]);
 
             const max = _.maxBy(this.props.series, i => i.value);
             this.scaleY = d3.scaleLinear()
-                .range([0, this.root.getBoundingClientRect().height])
+                .range([0, this.getBounds().height])
                 .domain([max ? max.value : 0, 0]);
 
-            this.svg.select('path').datum(this.props.series).attr('d', this.line);
+            this.svg.selectAll('path').remove();
+            this.svg.append('path')
+                .datum(this.props.series)
+                .attr('fill', 'none')
+                .attr('stroke', 'steelblue')
+                .attr('stroke-linejoin', 'round')
+                .attr('stroke-linecap', 'round')
+                .attr('stroke-width', 1.5)
+                .attr('d', this.line);
+
+            // Update the X gridlines
+            this.gridX.attr('transform', 'translate(0,' + this.getBounds().height + ')')
+                .call(
+                    d3.axisBottom(this.scaleX)
+                        .tickSize(-this.getBounds().height)
+                        .ticks(Math.floor(this.getBounds().width / 10))
+
+                );
+
+            // Update the Y gridlines
+            this.gridY.call(
+                d3.axisLeft(this.scaleY)
+                    .tickSize(-this.getBounds().width)
+                    .ticks(Math.floor(this.getBounds().height / 10))
+            );
         }
     }
 
