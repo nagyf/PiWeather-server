d5c6ce14365757bf847b3c43ad4f2f403377de05
Ferenc Nagy <nagyf@nagyf.hu>
2017-03-09 19:30:22 +0100

    Added mouse hover events to display the value in the given position

--- src/client/components/line-chart/index.jsx
+++ src/client/components/line-chart/index.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import * as d3 from 'd3';
 import './line-chart.less';
 import * as _ from 'lodash';
@@ -16,6 +16,74 @@ class LineChart extends React.Component {
             })
             .y((d) => {
                 return this.scaleY(d.value);
+            });
+
+        this.chartG = this.svg.append('g')
+            .attr('class', 'chart-content');
+
+        this.mouseG = this.svg.append('g')
+            .attr('class', 'mouse-over-effects');
+
+        this.mouseG.append('path') // this is the black vertical line to follow mouse
+            .attr('class', 'mouse-line')
+            .style('stroke', 'black')
+            .style('stroke-width', '1px')
+            .style('opacity', '0');
+
+        this.mouseG.append('circle') // this is the black vertical line to follow mouse
+            .attr('r', 7)
+            .style('stroke', 'green')
+            .style('fill', 'none')
+            .style('stroke-width', '1px')
+            .style('opacity', '0');
+
+        this.mouseG.append('text').style('opacity', '0');
+
+        const that = this;
+        this.mouseG.append('rect') // append a rect to catch mouse movements on canvas
+            .attr('width', '100%') // can't catch mouse events on a g element
+            .attr('height', '100%')
+            .attr('fill', 'none')
+            .attr('pointer-events', 'all')
+            .on('mouseout', () => { // on mouse out hide line, circles and text
+                that.mouseG.select('.mouse-line')
+                    .style('opacity', '0');
+                that.mouseG.selectAll('circle').style('opacity', 0);
+                that.mouseG.selectAll('text').style('opacity', 0);
+            })
+            .on('mouseover', () => { // on mouse in show line, circles and text
+                that.mouseG.select('.mouse-line')
+                    .style('opacity', '1');
+                that.mouseG.selectAll('circle').style('opacity', 1);
+                that.mouseG.selectAll('text').style('opacity', 1);
+            })
+            .on('mousemove', function () { // mouse moving over canvas
+                const mouse = d3.mouse(this);
+                let idx = Math.round(that.scaleX.invert(mouse[0]));
+                idx = that.props.series.length > idx ? idx : that.props.series.length - 1;
+                const d = that.props.series[idx];
+
+                that.mouseG.select('.mouse-line')
+                    .attr('d', () => {
+                        let d = 'M' + mouse[0] + ',' + that.getBounds().height;
+                        d += ' ' + mouse[0] + ',' + 0;
+                        return d;
+                    });
+                that.mouseG.select('circle').attr('cx', mouse[0]).attr('cy', that.scaleY(d.value));
+                that.mouseG.select('text').attr('x', function () {
+                    const length = this.getComputedTextLength();
+                    const x = d3.mouse(this)[0];
+                    let newX = x + 10;
+                    if (newX + length > that.getBounds().width) {
+                        newX = x - 10 - length;
+                    }
+                    return newX;
+                }).attr('y', function () {
+                    return that.scaleY(d.value) + 20;
+                }).text(`${d.value.toFixed(2)} ${d.unit}`)
+                    .each(function () {
+
+                    });
             });
 
         this.bound_renderChart = this.renderChart.bind(this);
@@ -45,8 +113,8 @@ class LineChart extends React.Component {
                 .range([0, this.getBounds().height])
                 .domain([max ? max.value : 0, 0]);
 
-            this.svg.selectAll('path').remove();
-            this.svg.append('path')
+            this.chartG.selectAll('path').remove();
+            this.chartG.append('path')
                 .datum(this.props.series)
                 .attr('fill', 'none')
                 .attr('stroke', 'steelblue')
@@ -61,7 +129,6 @@ class LineChart extends React.Component {
                     d3.axisBottom(this.scaleX)
                         .tickSize(-this.getBounds().height)
                         .ticks(Math.floor(this.getBounds().width / 10))
-
                 );
 
             // Update the Y gridlines
